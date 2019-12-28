41960bfe7cfe490fbc1e50103875c4d910bb76b3
Abhik Banerjee <abhik.banerjee.1999@gmail.com>
2019-12-28 16:17:10 +0530

    Trying to deploy to Azure.

--- contracts/Feedback.sol
+++ contracts/Feedback.sol
@@ -1,31 +1,50 @@
-pragma solidity ^0.6.0;
+pragma solidity ^0.5.0;
 
 //Smart contract for Decentralized Feedback System
-contract MyContract{
+contract MyContract
+{
 
 
     //to check whether the Person has voted or not
     enum State {NONE,VOTED}
 
+    /* Fill in the keyword. Hint: We want to protect our users balance from other contracts*/
+    mapping (address => uint) private balances;
+
+    //mapping of the address to the policymaker
+    mapping (address => Policymaker) policymakers;
+
+    //mapping of the address to the person
+    mapping (address => Policy) public policies;
+
+    //mapping of the address to the policy
+    mapping(address => Person) public persons;
+
+    //store accounts that has given feedback
+    mapping(address => bool) public voters;
+
     //to check whether the Contract is active or not
     enum contractState {ACTIVE,INACTIVE}
 
     //initially set to inactive
-    contractState state = contractState.INACTIVE;
+    contractState state=contractState.INACTIVE;
 
     address private commisioner;
 
-    event votedEvent (
-        address indexed _policyAddress
-    );
+    event votedEvent (address indexed _policyAddress);
 
-    event added(string _message, address indexed _address);
+    //setting the state to active
+    constructor() public
+    {
+        commisioner = msg.sender;
+        state = contractState.ACTIVE;
+
+    }
 
     //defining structure of the Policymaker
     struct Policymaker
     {
-
-        string name;
+        bytes32 name;
         uint id;
         uint constituency;
         bool exists;
@@ -37,7 +56,6 @@ contract MyContract{
         bytes32 details; //stores only the hash of the actual statement
         bytes32 beneficiary; //stores only the hash of the actual beneficiary criteria
         address framer;
-        uint creditPoints;
         uint voteCountFor;
         uint votecountAgainst;
         bool exists;
@@ -55,47 +73,28 @@ contract MyContract{
     struct Person
     {
         uint id;
-        string name;
+        bytes32 name;
         //initializing a dynamic array for the feedbacks
         //stores the feedback everytime the person votes in for a policy with the profile
-        uint credits;
         uint numPolcies; //counts the number of policies the person is enrolled in
         Feedback[] feedbacks;
         bool exists;
     }
 
-    //mapping of the address to the policymaker
-    mapping (address => Policymaker) public policymakers;
-
-    //mapping of the address to the person
-    mapping (address => Policy) public policies;
-
-    //mapping of the address to the policy
-    mapping(address => Person) public persons;
-
-    //setting the state to active
-    constructor() public
-    {
-        commisioner = msg.sender;
-        state = contractState.ACTIVE;
-
-    }
 
     //setting up the permissions an passing thee _personAddress as a key
     //initializing the parameters of the function
     //mapping of the address to the Person
-    function addPerson(uint _id, string memory _name, address _personAddress) public
+    function addPerson(uint _id, bytes32 _name, address _personAddress) public
     {
-        require(msg.sender == commisioner, "Only Commisioner can take this action!");
-        require(!persons[_personAddress].exists,"Person already exists!");
+        require(msg.sender == commisioner, "Only Comissioner can take this action!");
+        require(!persons[_personAddress].exists);
 
         persons[_personAddress].id = _id;
         persons[_personAddress].name = _name;
         persons[_personAddress].numPolcies = 0;
-        persons[_personAddress].credits = 0;
         persons[_personAddress].exists = true;
 
-        emit added("Added Person into the system", _personAddress);
 
     }
 
@@ -103,66 +102,80 @@ contract MyContract{
         //initializing the parameters of the function
         //mapping of the address to the Policymaker
 
-    function addPolicymaker(address _policymakerAddress,string memory _name, uint _id,uint _constituency) public
+    function addPolicymaker(address _policymakerAddress,bytes32 _name, uint _id,uint _constituency) public
     {
-        require(msg.sender == commisioner, "Only the commisioner can take this action!");
-        require(!policymakers[_policymakerAddress].exists, "Policymaker already exists in the system!");
+        require(msg.sender==commisioner);
+        require(!policymakers[_policymakerAddress].exists);
 
-        policymakers[_policymakerAddress].name = _name;
-        policymakers[_policymakerAddress].id = _id;
-        policymakers[_policymakerAddress].constituency = _constituency;
-        policymakers[_policymakerAddress].exists = true;
-
-        emit added("Added PolicyMaker to the system", _policymakerAddress);
+        policymakers[_policymakerAddress].name=_name;
+        policymakers[_policymakerAddress].id=_id;
+        policymakers[_policymakerAddress].constituency=_constituency;
+        policymakers[_policymakerAddress].exists=true;
     }
 
         //setting up the permissions an passing thee _personAddress as a key
         //initializing the parameters of the function
         //mapping of the address to the Policy
 
-    function addPolicy( address _policyAddress,address _framer, bytes32 _beneficiary, bytes32 _details, uint _credits) public
+    function addPolicy( address _policyAddress,address _framer, bytes32 _beneficiary, bytes32 _details) public
     {
-        require(msg.sender == commisioner, "Only the commisioner can take this action!");
-        require(policymakers[_framer].exists, "Policy maker must exist in the system!");
-        require(!policies[_policyAddress].exists, "Policy already exists at this address!");
+        require(msg.sender == commisioner);
+        require(policymakers[_framer].exists);
+        require(!policies[_policyAddress].exists);
 
 
-        policies[_policyAddress].details = _details;
-        policies[_policyAddress].beneficiary = _beneficiary;
-        policies[_policyAddress].framer = _framer;
-        policies[_policyAddress].creditPoints = _credits;
-        policies[_policyAddress].exists = true;
+        policies[_policyAddress].details=_details;
+        policies[_policyAddress].beneficiary=_beneficiary;
+        policies[_policyAddress].framer=_framer;
+        policies[_policyAddress].exists=true;
 
-        emit added("Added Policy to the System", _policyAddress);
     }
 
-    //enrolls person into a Policy
-    function enrollPerson(address _policyAddress,address _personAddress) public {
-        require(msg.sender == commisioner, "Only Commisioner can do this!");
-        require(policies[_policyAddress].exists,"Policy must exist!");
-        require(persons[_personAddress].exists, "Person must exist!");
+    function enrollPerson(address _policyAddress,address _personAddress) public returns (uint)
+     {
+        require(msg.sender==commisioner);
+        require(policies[_policyAddress].exists);
+        require(persons[_personAddress].exists);
 
         persons[_personAddress].numPolcies++;
         persons[_personAddress].feedbacks.push(Feedback(_policyAddress, State.NONE));
     }
 
+    function crediting() public payable returns (uint) {
+      /* Add the amount to the user's balance, call the event associated with a deposit,
+        then return the balance of the user */
+      address user = msg.sender;
+
+      balances[user] += msg.value;
+
+      emit votedEvent(user);
+
+      return balances[user];
+  }
+
+  function balance() public view returns (uint) {
+      /* Get the balance of the sender of this transaction */
+      address user = msg.sender;
+
+      return balances[user];
+  }
+
+
+
     function vote(address _policyAddress) public
     {
       //require person has not voted before
-      require(persons[msg.sender].exists, "Only the beneficiary can vote for the Feedback!");
-      require(policies[_policyAddress].exists, "Policy must exist in the system!");
+      require(persons[msg.sender].exists);
+      require(policies[_policyAddress].exists);
 
       uint i;
-      for(i = 0;i<persons[msg.sender].numPolcies;i++){
+      for(i=0;i<persons[msg.sender].numPolcies;i++){
 
           //check if the person is enrolled in the policy and has not yet voted.
           if(persons[msg.sender].feedbacks[i].policy == _policyAddress && persons[msg.sender].feedbacks[i].feedback == State.NONE)
-          {
-            persons[msg.sender].feedbacks[i].feedback = State.VOTED;
-            persons[msg.sender].credits += policies[_policyAddress].creditPoints;
-          }
+          persons[msg.sender].feedbacks[i].feedback = State.VOTED;
           else
-          revert("The Beneficiary is not enrolled in the Policy.");
+          revert();
       }
 
       // trigger voted event
--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,18 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const rpc_endpoint = "http://ethjnxov6-dns-reg1.centralus.cloudapp.azure.com:8540";
+const mnemonic = "camera mother song regret edge fabric lady spy genuine remove peasant keen";
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // for more about customizing your Truffle configuration!
   networks: {
     development: {
-      host: "127.0.0.1",
+      host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
     },
-    develop: {
-      port: 8545
+    poa: {
+      provider: new HDWalletProvider(mnemonic, rpc_endpoint),
+      network_id: 10101010,
+      gasPrice : 0
     }
   },
   compilers: {
